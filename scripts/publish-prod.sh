cb47f5011ad3d28f4e9afbf67de08a67a7f05fa1
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-08-21 14:20:49 +0300

    added git tags

--- scripts/publish-prod.sh
+++ scripts/publish-prod.sh
@@ -2,5 +2,8 @@
 
 PKG_VERSION=`node -p "require('./package.json').version"`
 S3PATH=s3://cdn.ownid.com/js
+
+git tag -a v$PKG_VERSION -m "Production release $PKG_VERSION"
+
 aws s3 cp ./dist $S3PATH --recursive
 
