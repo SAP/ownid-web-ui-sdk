a3f89d13dfcf78df3fc139a924e3c50ef8921041
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-08-21 14:00:12 +0300

    moved dev and staging to cdn.ownid.com

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,11 +3,13 @@
 ENV=$1
 
 PKG_VERSION=`node -p "require('./package.json').version"`
-S3PATH=s3://ownid-sdks-bucket/$ENV/web-ui-sdk
+S3PATH=s3://cdn.ownid.com/$ENV/js
 FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
 
-echo Path: $S3PATH/$FOLDER
+echo Version folder: $S3PATH/$FOLDER
+echo Latest folder: $S3PATH/latest
 
 aws s3 cp ./dist $S3PATH/latest --recursive
 aws s3 cp ./dist $S3PATH/$FOLDER --recursive
+
 
