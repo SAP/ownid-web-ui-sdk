0d1d3c338ff0efcc6c7513c85024472026ad8b57
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-08-21 14:35:16 +0300

    added tags push

--- scripts/publish-prod.sh
+++ scripts/publish-prod.sh
@@ -3,7 +3,9 @@
 PKG_VERSION=`node -p "require('./package.json').version"`
 S3PATH=s3://cdn.ownid.com/js
 
-git tag -a prod/$PKG_VERSION -m "Production release $PKG_VERSION"
+GIT_TAG=prod/$PKG_VERSION
+git tag -a $GIT_TAG -m "Production release $PKG_VERSION"
+git push origin $GIT_TAG
 
 aws s3 cp ./dist $S3PATH --recursive
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,9 @@ PKG_VERSION=`node -p "require('./package.json').version"`
 S3PATH=s3://cdn.ownid.com/$ENV/js
 FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
 
-git tag $ENV/$FOLDER
+GIT_TAG=$ENV/$FOLDER
+git tag $GIT_TAG
+git push origin $GIT_TAG
 
 echo Version folder: $S3PATH/$FOLDER
 echo Latest folder: $S3PATH/latest
