3014c2a1136a6fef180d9b1996b415aac0ab8b14
Vitalii Marynkevych <vitalii.marynkevych@sap.com>
2020-09-03 14:59:23 +0300

    fixed (#113)

--- scripts/set-build-num.sh
+++ scripts/set-build-num.sh
@@ -4,10 +4,10 @@ BUILD_NUM=$1
 
 echo 'Adding build number '$BUILD_NUM' to version'
 
-VERSION=`node -p "require('./package.json').version"`
-
 #updating package.json
 sed -i -r -e 's/(\"version\": \"[0-9]{1,2}\.[0-9]{1,3}\.)[0-9]*([A-Za-z0-9\-]*\")/\1'$BUILD_NUM'\2/gm' ./package.json
 
-#updating index.html
-sed -i -r -e 's/\$\{APP-VERSION\}/'$BUILD_NUM'/gm' ./src/ownid-web-ui-sdk.ts
+VERSION=`node -p "require('./package.json').version"`
+
+#updating ownid-web-ui-sdk.ts
+sed -i -r -e 's/\$\{APP_VERSION\}/'$VERSION'/gm' ./src/ownid-web-ui-sdk.ts
