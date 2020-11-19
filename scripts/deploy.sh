63c6fe6f437185246aa42be8f6a33a8cfcc4bd7d
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-11-19 17:12:51 +0200

    added cdn bucket deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 
 ENV=$1
+CDN_DISTRIBUITION=$2
 
-PKG_VERSION=`node -p "require('./package.json').version"`
-S3PATH=s3://cdn.ownid.com/$ENV/js
+#PKG_VERSION=`node -p "require('./package.json').version"`
 #FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
 
 # GIT_TAG=$ENV/$FOLDER
@@ -11,12 +11,9 @@ S3PATH=s3://cdn.ownid.com/$ENV/js
 # git push origin $GIT_TAG
 
 #echo Version folder: $S3PATH/$FOLDER
-echo Latest folder: $S3PATH/latest
 
-aws s3 cp ./dist $S3PATH/latest --recursive
-#aws s3 cp ./dist $S3PATH/$FOLDER --recursive
-
-DISTRIBUTIONID=EDUOQUBKTEHZF
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTIONID --paths "/${ENV}/js/latest/*"
+S3PATH=s3://cdn.$ENV.ownid.com/js
+aws s3 cp ./dist $S3PATH --recursive
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUITION --paths "/*"
 
 
