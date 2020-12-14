96b1ce744176de4abe0a685b4f8e107f5472a94e
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-12-14 17:56:40 +0200

    added github actions support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,7 @@
-#!bin/sh
+#!bin/bash
 
-ENV=$1
-CDN_DISTRIBUITION=$2
+S3_BUCKET=$1
+DISTRIBUTIONID=$2
 
-#PKG_VERSION=`node -p "require('./package.json').version"`
-#FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
-
-# GIT_TAG=$ENV/$FOLDER
-# git tag $GIT_TAG
-# git push origin $GIT_TAG
-
-#echo Version folder: $S3PATH/$FOLDER
-
-S3PATH=s3://cdn.$ENV.ownid.com/js
-aws s3 cp ./dist $S3PATH --recursive
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUITION --paths "/*"
-
-
+aws s3 sync ./dist $S3_BUCKET --delete
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTIONID --paths "/js/*"
